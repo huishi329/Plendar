2a0f9f5a728230e75523ab5ade2e71cb6931f0aa
Huishi <anhuishi95@gmail.com>
2022-12-21 10:23:58 -0800

    Make ProfileDropdown respond to click event

--- react-app/src/components/Navbar/ProfileDropdown/ProfileDropdown.js
+++ react-app/src/components/Navbar/ProfileDropdown/ProfileDropdown.js
@@ -1,14 +1,20 @@
 import styles from "./ProfileDropdown.module.css";
 import { useDispatch } from 'react-redux';
 import { signOut } from '../../../store/session';
+import { useEffect } from "react";
 
 export default function ProfileDropdown({ user, setShowDropdown }) {
     const dispatch = useDispatch();
-
     const onClickSignOut = () => {
         dispatch(signOut());
         setShowDropdown(false);
     };
+    useEffect(() => {
+        const closeDropdown = () => setShowDropdown(false);
+        document.addEventListener('click', closeDropdown)
+        return () => document.removeEventListener('click', closeDropdown)
+    }, [setShowDropdown])
+
 
     return <>
         <div className={styles.wrapper}>
