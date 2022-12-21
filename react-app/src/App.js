ec6d6130feaf1ab47ea9b135abb42cc75be032f2
Huishi <anhuishi95@gmail.com>
2022-12-20 16:07:48 -0800

    Initial App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-import { authenticate } from './store/session';
+
+import { restoreUser } from './store/session';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async() => {
-      await dispatch(authenticate());
+    (async () => {
+      await dispatch(restoreUser());
       setLoaded(true);
     })();
   }, [dispatch]);
@@ -28,18 +24,7 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
+
         <Route path='/' exact={true} >
           <h1>My Home Page</h1>
         </Route>
