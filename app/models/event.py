dd555225ace0c6c7b5c63d41089240022dfb8700
Huishi <anhuishi95@gmail.com>
2022-12-21 16:49:13 -0800

    Create Event Modal

---
+++ app/models/event.py
@@ -0,0 +1,44 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Integer, DateTime, VARCHAR
+from sqlalchemy.sql import func
+
+
+class Event(db.Model):
+    __tablename__ = "events"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = Column(Integer, primary_key=True)
+
+    calendar_id = Column(Integer, ForeignKey(
+        add_prefix_for_prod('calendars.id'), name='fk_event_calendar_id'),
+        nullable=False)
+    title = Column(VARCHAR, server_default='(No title)')
+    address = Column(VARCHAR)
+    description = Column(VARCHAR)
+    start = Column(DateTime(timezone=True),  nullable=False)
+    end = Column(DateTime(timezone=True),  nullable=False)
+    reccurence = Column(Integer)
+
+    created_at = Column(DateTime(timezone=True),
+                        server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True),
+                        server_default=func.now(), onupdate=func.now(),
+                        nullable=False)
+
+    def to_dict(self):
+        return {
+            "calendar_id": self.calendar_id,
+            "title": self.title,
+            "address": self.address,
+            "description": self.description,
+            "start": self.start,
+            "end": self.end,
+            "reccurence": self.reccurence
+        }
