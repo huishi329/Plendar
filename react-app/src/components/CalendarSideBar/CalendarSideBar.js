9b8be7aa86706212b258a050fafc8a0a16529119
Huishi <anhuishi95@gmail.com>
2022-12-23 19:43:08 -0800

    Add MyCalendar component and style it

--- react-app/src/components/CalendarSideBar/CalendarSideBar.js
+++ react-app/src/components/CalendarSideBar/CalendarSideBar.js
@@ -1,8 +1,15 @@
 import styles from './CalendarSideBar.module.css'
+import MyCalendars from './MyCalendars/MyCalendars'
+
 
 export default function CalendarSideBar() {
+
+
+
     return (
-        <div className={styles.Wrapper}
-        >CalendarSideBar placeholder</div>
+        <div className={styles.Wrapper}>
+            <MyCalendars />
+
+        </div>
     )
 }
---
+++ react-app/src/components/CalendarSideBar/MyCalendars/MyCalendars.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCalendars } from "../../../store/calendars";
+import CalendarItem from '../CalendarItem/CalendarItem'
+import styles from './MyCalendars.module.css'
+
+export default function MyCalendars() {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.session.user)
+    const calendars = useSelector(state => state.calendars)
+
+    useEffect(() => {
+        if (user) dispatch(getCalendars())
+    }, [dispatch, user])
+
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.title}>My calendars</div>
+            {calendars && Object.values(calendars).map(calendar => (<CalendarItem calendar={calendar} />))}
+        </div>
+    )
+}
