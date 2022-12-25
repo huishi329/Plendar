bdda02bb8bfc63cf17d5bd8048fb9eeee424dba2
Huishi <anhuishi95@gmail.com>
2022-12-24 22:14:05 -0800

    Debug EventItem datetime bug

--- react-app/src/components/MonthView/DayTile/DayTile.js
+++ react-app/src/components/MonthView/DayTile/DayTile.js
@@ -3,11 +3,18 @@ import styles from './DayTile.module.css'
 import EventItem from './EventItem/EventItem';
 
 export default function DayTile({ date }) {
+    const user = useSelector(state => state.session.user)
     const events = useSelector(state => Object.values(state.events));
     // filter to get the events for the date
-    const day_events = events?.filter(event =>
-        new Date(event.start_time).getDate() === date.getDate() ||
-        (event.recurrence === 1 && date.getDate() >= new Date(event.start_time).getDate()))
+    const day_events = events?.filter(event => {
+        // envents that doesn't repeat
+        const eventDate = new Date(event.start_time);
+        return (eventDate.getDate() === date.getDate() ||
+            // events that repeat daily
+            (event.recurrence === 1 && date.getDate() >= eventDate.getDate()) ||
+            // events that repeat weekly
+            (event.recurrence === 7 && date.getDate() >= eventDate.getDate() && date.getDay() === eventDate.getDay()))
+    })
     // Make a deep copy and set date to the DayTile date
     const day_events_sorted = JSON.parse(JSON.stringify(day_events)).sort((a, b) => {
         a.start_time = new Date(a.start_time)
@@ -16,6 +23,7 @@ export default function DayTile({ date }) {
         b.start_time.setDate(date.getDate())
         return a.start_time - b.start_time
     })
+    // if (!user || !events.length) return null;
 
     return (
         <div className={styles.wrapper}>
--- react-app/src/components/MonthView/DayTile/EventItem/EventItem.js
+++ react-app/src/components/MonthView/DayTile/EventItem/EventItem.js
@@ -1,12 +1,13 @@
 import styles from './EventItem.module.css'
 
 export default function EventItem({ event }) {
+    console.log(event);
     return (<div className={styles.wrapper}>
         <div className={styles.content}>
             <div className={styles.dot}>
                 <i className="fa-solid fa-circle"></i>
             </div>
-            {`${event.start_time.toLocaleTimeString(
+            {`${new Date(event.start_time).toLocaleTimeString(
                 'en-US',
                 { hour: '2-digit', minute: '2-digit' })
                 .toLowerCase()
