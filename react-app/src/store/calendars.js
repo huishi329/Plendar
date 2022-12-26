fb3a7f3fc4fc2b26b7a25b2aef8871bed8732a01
Huishi <anhuishi95@gmail.com>
2022-12-25 20:31:32 -0800

    Delete excessive dispatch

--- react-app/src/store/calendars.js
+++ react-app/src/store/calendars.js
@@ -1,5 +1,5 @@
 import { csrfFetch } from './csrf';
-import { getEvents, removeEvents } from './events';
+import { removeEvents } from './events';
 
 const GET_CALENDARS = 'calendars/getCalendars'
 
@@ -17,8 +17,7 @@ export const toggleCalendar = (calendar_id) => async dispatch => {
         method: "PATCH"
     });
     const calendar = await response.json();
-    if (calendar.is_displayed) dispatch(getEvents(calendar.id))
-    else dispatch(removeEvents(calendar.id))
+    if (!calendar.is_displayed) dispatch(removeEvents(calendar.id))
     dispatch({ type: TOGGLE_CALENDAR, calendar });
 }
 
