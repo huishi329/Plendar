601d27c3bdc64ef6f804746e97c2868083029503
Huishi <anhuishi95@gmail.com>
2022-12-25 19:01:08 -0800

    Add onChange handler for checkbox

--- react-app/src/components/CalendarSideBar/CalendarItem/CalendarItem.js
+++ react-app/src/components/CalendarSideBar/CalendarItem/CalendarItem.js
@@ -1,16 +1,19 @@
+import { useDispatch } from 'react-redux'
+import { toggleCalendar } from '../../../store/calendars'
 import styles from './CalendarItem.module.css'
 
 export default function CalendarItem({ calendar }) {
-    const handleCalendarVisibility = (calendar_id) => {
-        return;
+    const dispatch = useDispatch()
+    const handleCalendarVisibility = () => {
+        dispatch(toggleCalendar(calendar.id))
     }
 
     return (
         <div className={styles.wrapper}>
             <input
                 type="checkbox"
-                checked={calendar.is_active}
-                onChange={() => handleCalendarVisibility(calendar.id)}
+                checked={calendar.is_displayed}
+                onChange={() => handleCalendarVisibility()}
             >
             </input>
             <label htmlFor={calendar.name}>{calendar.name}</label>
