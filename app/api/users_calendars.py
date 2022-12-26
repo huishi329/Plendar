0769883e0929e6f8079d26fc4ef87ab342186788
Huishi <anhuishi95@gmail.com>
2022-12-25 16:05:10 -0800

    Use current_user of flask_login for toggle_calendar route

--- app/api/users_calendars.py
+++ app/api/users_calendars.py
@@ -4,11 +4,11 @@ from app.models import db, User, Calendar, UserCalendar
 
 bp = Blueprint('users_calendars', __name__, url_prefix="/users_calendars")
 
-@bp.route("/<int:user_id>/<int:calendar_id>", methods=['PATCH'])
+@bp.route("/current/<int:calendar_id>", methods=['PATCH'])
 @login_required
-def toggle_calendar(user_id, calendar_id):
+def toggle_calendar(calendar_id):
     user_calendar = UserCalendar.query.filter(
-        UserCalendar.user_id == user_id,
+        UserCalendar.user_id == current_user.id,
         UserCalendar.calendar_id == calendar_id).first();
     print(user_calendar)
     user_calendar.is_displayed = not user_calendar.is_displayed
