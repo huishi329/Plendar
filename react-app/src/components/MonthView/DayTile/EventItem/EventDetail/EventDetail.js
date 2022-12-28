af1f4e071e08437e76b67068ba843e82f8f1f21e
Huishi <anhuishi95@gmail.com>
2022-12-27 19:42:17 -0800

    Fill EventDetail content

--- react-app/src/components/MonthView/DayTile/EventItem/EventDetail/EventDetail.js
+++ react-app/src/components/MonthView/DayTile/EventItem/EventDetail/EventDetail.js
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentEvent } from '../../../../../store/modals';
 import styles from './EventDetail.module.css'
 
 export default function EventDetail({ event, x, y }) {
     const eventRef = useRef();
     const dispatch = useDispatch();
+    const calendars = useSelector(state => state.calendars)
 
     useEffect(() => {
         const closeEventDetail = (e) => {
@@ -23,15 +24,28 @@ export default function EventDetail({ event, x, y }) {
                 <i className="fa-solid fa-square"></i>
                 {event.title}
             </div>
-            <div>
-                {new Date(event.start_time).toLocaleDateString(
-                    'en-US',
+            <div className={styles.datetime}>
+                {event.start_time.toLocaleDateString('en-US',
                     { weekday: 'long', day: '2-digit', month: 'long' })
                 }
+                <i class="fa-solid fa-circle"></i>
+                {event.start_time.toLocaleTimeString('en-US',
+                    { hour: '2-digit', minute: '2-digit' }).toLowerCase().split(" ").join("")}
+                {` - `}
+                {event.end_time.toLocaleTimeString('en-US',
+                    { hour: '2-digit', minute: '2-digit' }).toLowerCase().split(" ").join("")}
             </div>
-            {event.description && <div>
+            {event.address && <div className={styles.address}>
+                <i className="fa-solid fa-location-dot"></i>
+                {event.address}
+            </div>}
+            {event.description && <div className={styles.description}>
                 <i className="fa-solid fa-bars"></i>
                 {event.description}
+            </div>}
+            {calendars && <div>
+                <i class="fa-regular fa-calendar"></i>
+                {calendars[event.calendar_id].name}
             </div>}
         </div >
     )
