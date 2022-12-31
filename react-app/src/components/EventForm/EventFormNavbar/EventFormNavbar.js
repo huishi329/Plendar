873fc0b9b805b973b0446b63041b4b4f718a5c21
Huishi <anhuishi95@gmail.com>
2022-12-30 19:26:35 -0800

    Add EventNavbar component

---
+++ react-app/src/components/EventForm/EventFormNavbar/EventFormNavbar.js
@@ -0,0 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { setCurrentDate } from '../../../store/modals';
+import styles from './EventFormNavbar.module.css'
+
+export function EventFormNavbar() {
+    const dispatch = useDispatch();
+    return (
+        <div className={styles.wrapper}>
+            <button
+                className={styles.button}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    dispatch(setCurrentDate(null))
+                }}>
+                <i className="fa-regular fa-circle-xmark"></i>
+            </button>
+        </div>
+    )
+}
