0b152348b25271d134fbe968d79dd2df32ed89b9
Huishi <anhuishi95@gmail.com>
2023-01-01 14:05:38 -0800

    Link EditEventForm to EventNavbar

--- react-app/src/components/MonthView/DayTile/EventItem/EventDetail/EventNavbar/EventNavbar.js
+++ react-app/src/components/MonthView/DayTile/EventItem/EventDetail/EventNavbar/EventNavbar.js
@@ -1,16 +1,22 @@
 import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom";
 import { deleteEvent } from "../../../../../../store/events";
 import { setCurrentEvent } from "../../../../../../store/modals";
 import styles from './EventNavbar.module.css'
 
 export default function EventNavbar({ event, calendar }) {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const user = useSelector(state => state.session.user);
 
     return (
         <div className={styles.wrapper}>
             {(user.id === calendar.owner_id) && <div>
-                <button className={styles.button}>
+                <button className={styles.button}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        navigate(`/eventedit/${event.id}`);
+                    }}>
                     <i className="fa-regular fa-pen-to-square"></i>
                 </button>
                 <button className={styles.button} onClick={() => dispatch(deleteEvent(event.id))}>
