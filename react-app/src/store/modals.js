287ea6da59714e5c52b7389f20cbeebd55a4cd62
Huishi <anhuishi95@gmail.com>
2023-01-04 10:30:18 -0800

    Add setDeleteCalendarModal thunk action

--- react-app/src/store/modals.js
+++ react-app/src/store/modals.js
@@ -1,5 +1,6 @@
 const SET_SIGNUP_MODAL = 'modals/setSignUpModal';
 const SET_SIGNIN_MODAL = 'modals/setSignInModal';
+const SET_DELETE_CALENDAR_MODAL = 'modals/setDeleteCalendarModal'
 const SET_PROFILE_DROPDOWN = 'modals/setProfileDropdown'
 const SET_CURRENT_EVENT = 'modals/setCurrentEvent';
 const SET_CURRENT_DATE = 'modals/setCurrentDate';
@@ -11,13 +12,16 @@ export const setSignInModal = showSignInModal => {
     return { type: SET_SIGNIN_MODAL, showSignInModal };
 };
 export const setCurrentEvent = event => {
-    return { type: SET_CURRENT_EVENT, event }
+    return { type: SET_CURRENT_EVENT, event };
 };
 export const setCurrentDate = date => {
-    return { type: SET_CURRENT_DATE, date }
+    return { type: SET_CURRENT_DATE, date };
 }
 export const setProfileDropdown = showProfileDropdown => {
-    return { type: SET_PROFILE_DROPDOWN, showProfileDropdown }
+    return { type: SET_PROFILE_DROPDOWN, showProfileDropdown };
+}
+export const setDeleteCalendarModal = showDeleteCalendarModal => {
+    return { type: SET_DELETE_CALENDAR_MODAL, showDeleteCalendarModal };
 }
 
 export default function modalsReducer(state = {}, action) {
@@ -26,6 +30,8 @@ export default function modalsReducer(state = {}, action) {
             return { ...state, showSignUpModal: action.showSignUpModal };
         case SET_SIGNIN_MODAL:
             return { ...state, showSignInModal: action.showSignInModal };
+        case SET_DELETE_CALENDAR_MODAL:
+            return { ...state, showDeleteCalendarModal: action.showDeleteCalendarModal };
         case SET_PROFILE_DROPDOWN:
             return { showProfileDropdown: action.showProfileDropdown }
         case SET_CURRENT_EVENT:
