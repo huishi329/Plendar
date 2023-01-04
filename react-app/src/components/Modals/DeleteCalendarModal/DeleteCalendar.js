69c1de6f6aa4ed78cba5d0beeb2b49c202fbce7a
Huishi <anhuishi95@gmail.com>
2023-01-04 11:39:02 -0800

    Add handleDelete function

--- react-app/src/components/Modals/DeleteCalendarModal/DeleteCalendar.js
+++ react-app/src/components/Modals/DeleteCalendarModal/DeleteCalendar.js
@@ -1,16 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux'
 import styles from './DeleteCalendar.module.css'
+import { setDeleteCalendarModal } from '../../../store/modals'
+import { deleteCalendar } from '../../../store/calendars';
+import { removeEvents } from '../../../store/events';
 
 export default function DeleteCalendar() {
+    const dispatch = useDispatch();
+    const calendar = useSelector(state => state.modals.calendar);
+    const handleDelete = () => {
+        dispatch(deleteCalendar(calendar.id))
+            .then(() => {
+                dispatch(removeEvents(calendar.id));
+                dispatch(setDeleteCalendarModal(false));
+            });
+    };
+
     return (
-        <div>
-            <div>
-                {`Are you sure you want to remove Huishi? You'll no longer have access to this calendar and its events. Other people with access to the calendar can continue to use it.`}
+        <div className={styles.wrapper}>
+            <div >
+                {`Are you sure you want to remove ${calendar.name}? You'll no longer have access to this calendar and its events. Other people with access to the calendar can continue to use it.`}
             </div>
-            <div>
-                <button>
+            <div className={styles.buttons}>
+                <button onClick={() => dispatch(setDeleteCalendarModal(false))}>
                     Cancel
                 </button>
-                <button>
+                <button className={styles.removeButton} onClick={handleDelete}>
                     Remove Calendar
                 </button>
             </div>
