127429a4b1cad77583939987eb551edf984453b5
Huishi <anhuishi95@gmail.com>
2023-01-04 09:39:01 -0800

    Clean console.log

--- react-app/src/components/EventForm/EventForm.js
+++ react-app/src/components/EventForm/EventForm.js
@@ -20,7 +20,6 @@ export default function EventForm({ date, x, y }) {
     const endTimeStr = (currentMinute < 30) ?
         `${((Number(currentHour) + 1) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false })}: 30`
         : `${((Number(currentHour) + 2) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false })}:00`
-    console.log(startTimeStr, endTimeStr);
 
     const [expandTimeOptions, setExpandTimeOptions] = useState(false);
     const [expandMoreOptions, setExpanMoreOptions] = useState(false);
