d4e4fa9c94915fae022ab8b6f15791f2fb929e1b
Huishi <anhuishi95@gmail.com>
2023-01-04 11:05:08 -0800

    Add setCurrentCalendar action

--- react-app/src/store/modals.js
+++ react-app/src/store/modals.js
@@ -4,6 +4,7 @@ const SET_DELETE_CALENDAR_MODAL = 'modals/setDeleteCalendarModal'
 const SET_PROFILE_DROPDOWN = 'modals/setProfileDropdown'
 const SET_CURRENT_EVENT = 'modals/setCurrentEvent';
 const SET_CURRENT_DATE = 'modals/setCurrentDate';
+const SET_CURRENT_CALENDAR = 'modals/setCurrentCalendar';
 
 export const setSignUpModal = showSignUpModal => {
     return { type: SET_SIGNUP_MODAL, showSignUpModal };
@@ -11,18 +12,21 @@ export const setSignUpModal = showSignUpModal => {
 export const setSignInModal = showSignInModal => {
     return { type: SET_SIGNIN_MODAL, showSignInModal };
 };
+export const setDeleteCalendarModal = showDeleteCalendarModal => {
+    return { type: SET_DELETE_CALENDAR_MODAL, showDeleteCalendarModal };
+};
+export const setProfileDropdown = showProfileDropdown => {
+    return { type: SET_PROFILE_DROPDOWN, showProfileDropdown };
+};
 export const setCurrentEvent = event => {
     return { type: SET_CURRENT_EVENT, event };
 };
 export const setCurrentDate = date => {
     return { type: SET_CURRENT_DATE, date };
-}
-export const setProfileDropdown = showProfileDropdown => {
-    return { type: SET_PROFILE_DROPDOWN, showProfileDropdown };
-}
-export const setDeleteCalendarModal = showDeleteCalendarModal => {
-    return { type: SET_DELETE_CALENDAR_MODAL, showDeleteCalendarModal };
-}
+};
+export const setCurrentCalendar = calendar => {
+    return { type: SET_CURRENT_CALENDAR, calendar }
+};
 
 export default function modalsReducer(state = {}, action) {
     switch (action.type) {
@@ -38,6 +42,8 @@ export default function modalsReducer(state = {}, action) {
             return { event: action.event };
         case SET_CURRENT_DATE:
             return { date: action.date };
+        case SET_CURRENT_CALENDAR:
+            return { ...state, calendar: action.calendar }
         default:
             return state;
     }
