0f86037af3cafe3d33f432cdf4a7de43aa31b992
Huishi <anhuishi95@gmail.com>
2023-01-04 15:23:48 -0800

    Connect to EditCalendarForm

--- react-app/src/components/Settings/SettingSidebar/SettingSidebar.js
+++ react-app/src/components/Settings/SettingSidebar/SettingSidebar.js
@@ -1,12 +1,11 @@
 import styles from './SettingSidebar.module.css'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-export default function SettingSidebar() {
+export default function SettingSidebar({ calendars }) {
     const location = useLocation();
     const path = location.pathname;
     const navigate = useNavigate();
-    const calendars = Object.values(JSON.parse(window.localStorage.getItem('plendar')));
-    console.log(typeof path, path.includes('createcalendar'));
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.addCalendar}>
@@ -17,8 +16,12 @@ export default function SettingSidebar() {
             </div>
             <div className={styles.myCalendar}>
                 <div className={styles.category}>Settings for my calendars</div>
-                {calendars.map(calendar => (<button className={`${styles.button} ${path.includes(`calendars/${calendar.id}`) ? styles.highlighted : ''}`}
-                    key={calendar.id}>{calendar.name}</button>))}
+                {calendars.map(calendar => (
+                    <button className={`${styles.button} ${path.includes(`calendar/${calendar.id}`) ? styles.highlighted : ''}`}
+                        key={calendar.id}
+                        onClick={() => navigate(`calendar/${calendar.id}`)}
+                    >{calendar.name}</button>
+                ))}
             </div>
         </div>
     )
