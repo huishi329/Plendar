d5bac765b1864d433d19a348485449d1398fbd78
Huishi <anhuishi95@gmail.com>
2023-01-06 16:45:56 -0800

    Add countdown for redirecting

--- react-app/src/components/PageNotFound/PageNotFound.js
+++ react-app/src/components/PageNotFound/PageNotFound.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './PageNotFound.module.css'
 
+
 export default function PageNotFound() {
+    const [count, setCount] = useState(10);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (count === 0) navigate("/");
+            setCount(count - 1);
+        }, 1000)
+        return () => clearInterval(interval);
+    }, [count, navigate]);
+
     return (
         <div className={styles.container}>
-            <h1>
-                Oops, you have reached the desert ...
-            </h1>
+            <div className={styles.text}>
+                <h1>
+                    Oops, you have reached the desert ...
+                </h1>
+                <NavLink className={styles.link} to='/'>You will be redirected to oasis in {count} seconds...</NavLink>
+            </div>
             <img src="https://media.tenor.com/rrMGOAL6NLMAAAAC/desert-carry.gif"></img>
         </div>
     )
