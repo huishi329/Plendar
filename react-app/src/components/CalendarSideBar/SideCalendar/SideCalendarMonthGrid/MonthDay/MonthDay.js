09dc8e83bcc31fd076296d86328c90f2f4812e06
Huishi <anhuishi95@gmail.com>
2023-01-08 13:02:01 -0800

    Style MonthDay

--- react-app/src/components/CalendarSideBar/SideCalendar/SideCalendarMonthGrid/MonthDay/MonthDay.js
+++ react-app/src/components/CalendarSideBar/SideCalendar/SideCalendarMonthGrid/MonthDay/MonthDay.js
@@ -1,12 +1,22 @@
 import styles from './MonthDay.module.css'
 
-export default function MonthDay({ date }) {
+export default function MonthDay({ date, sideCalendarYear, sideCalendarMonth }) {
     const isClicked = false;
+    const isCurrentMonth = date.getFullYear() === sideCalendarYear && date.getMonth() === sideCalendarMonth;
+    const currentDate = new Date();
+    date.setHours(0, 0, 0, 0);
+    currentDate.setHours(0, 0, 0, 0);
+    const isCurrentDate = currentDate.getTime() === date.getTime();
     const handleClick = () => {
     }
 
     return (
-        <div className={`${styles.wrapper} ${isClicked ? styles.clicked : ''}`} onClick={handleClick}>
+        <div className={`
+        ${styles.wrapper}
+        ${isClicked && styles.clicked}
+        ${isCurrentMonth && styles.currentMonth}
+        ${isCurrentDate && styles.currentDate}
+        `} onClick={handleClick}>
             {`${date.getDate()} `}
         </div>
     )
