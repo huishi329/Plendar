5ab37566abb43e96aca32e8934e1086ac36d50ef
Huishi <anhuishi95@gmail.com>
2023-01-08 14:49:40 -0800

    Add createEvent button

--- react-app/src/components/CalendarSideBar/CalendarSideBar.js
+++ react-app/src/components/CalendarSideBar/CalendarSideBar.js
@@ -1,13 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentDate } from '../../store/modals';
 import styles from './CalendarSideBar.module.css'
 import MyCalendars from './MyCalendars/MyCalendars'
 import SideCalendar from './SideCalendar/SideCalendar'
 
 export default function CalendarSideBar() {
+    const dispatch = useDispatch();
     const user = useSelector(state => state.session.user);
+    const sideCalendarDate = useSelector(state => state.sessionData.sideCalendarDate);
+    const handleClick = () => {
+        dispatch(setCurrentDate(sideCalendarDate));
+    }
 
     return (
-        <div className={styles.Wrapper}>
+        <div className={styles.wrapper}>
+            {user && <button className={styles.create} onClick={handleClick}>
+                <i className="fa-solid fa-plus"></i>
+                Create
+            </button>}
             <SideCalendar />
             {user && <MyCalendars />}
         </div>
