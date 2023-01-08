7ee91b8b86d02de69a253b173f2b084bca93b1c0
Huishi <anhuishi95@gmail.com>
2023-01-08 13:06:09 -0800

    Add SideCalendar

--- react-app/src/components/CalendarSideBar/CalendarSideBar.js
+++ react-app/src/components/CalendarSideBar/CalendarSideBar.js
@@ -1,11 +1,12 @@
 import styles from './CalendarSideBar.module.css'
 import MyCalendars from './MyCalendars/MyCalendars'
-
+import SideCalendar from './SideCalendar/SideCalendar'
 
 export default function CalendarSideBar() {
 
     return (
         <div className={styles.Wrapper}>
+            <SideCalendar />
             <MyCalendars />
         </div>
     )
