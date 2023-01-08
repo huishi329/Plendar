2562b20703289aa58933e7a22717e72b5a055bb6
Huishi <anhuishi95@gmail.com>
2023-01-08 13:27:45 -0800

    Make calendar responsive to click event

--- react-app/src/components/CalendarSideBar/SideCalendar/SideCalendarMonthGrid/MonthDay/MonthDay.js
+++ react-app/src/components/CalendarSideBar/SideCalendar/SideCalendarMonthGrid/MonthDay/MonthDay.js
@@ -1,14 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setMonth, setSideCalendarDate, setSideCalendarMonth, setSideCalendarYear, setYear } from '../../../../../store/sessionData';
 import styles from './MonthDay.module.css'
 
 export default function MonthDay({ date, sideCalendarYear, sideCalendarMonth }) {
-    const isClicked = false;
+    const dispatch = useDispatch();
+    const sideCalendarDate = useSelector(state => state.sessionData.sideCalendarDate);
     const isCurrentMonth = date.getFullYear() === sideCalendarYear && date.getMonth() === sideCalendarMonth;
     const currentDate = new Date();
     date.setHours(0, 0, 0, 0);
     currentDate.setHours(0, 0, 0, 0);
     const isCurrentDate = currentDate.getTime() === date.getTime();
+    const isClicked = sideCalendarDate === date;
     const handleClick = () => {
-    }
+        dispatch(setSideCalendarDate(date));
+        if (!isCurrentMonth) {
+            const newMonth = date.getMonth();
+            const newYear = date.getFullYear();
+            dispatch(setMonth(newMonth));
+            dispatch(setYear(newYear));
+            dispatch(setSideCalendarMonth(newMonth));
+            dispatch(setSideCalendarYear(newYear));
+        }
+    };
 
     return (
         <div className={`
