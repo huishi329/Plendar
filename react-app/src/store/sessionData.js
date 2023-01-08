64170e276a9c4c4411f75bc61bde80422157d814
Huishi <anhuishi95@gmail.com>
2023-01-08 13:29:33 -0800

    Add actions for SideCalendar

--- react-app/src/store/sessionData.js
+++ react-app/src/store/sessionData.js
@@ -1,15 +1,25 @@
 
 const SET_YEAR = 'sessionData/setYear';
+const SET_MONTH = 'sessionData/setMonth';
+const SET_SIDE_CALENDAR_YEAR = 'sessionData/setSideCalendarYear';
+const SET_SIDE_CALENDAR_MONTH = 'sessionData/setSideCalendarMonth';
+const SET_SIDE_CALENDAR_DATE = 'sessionData/setSideCalendarDate';
 
 export const setYear = year => {
-    return { type: SET_YEAR, year }
-}
-
-const SET_MONTH = 'sessionData/setMonth';
-
+    return { type: SET_YEAR, year };
+};
 export const setMonth = month => {
-    return { type: SET_MONTH, month }
-}
+    return { type: SET_MONTH, month };
+};
+export const setSideCalendarYear = year => {
+    return { type: SET_SIDE_CALENDAR_YEAR, year };
+};
+export const setSideCalendarMonth = month => {
+    return { type: SET_SIDE_CALENDAR_MONTH, month };
+};
+export const setSideCalendarDate = date => {
+    return { type: SET_SIDE_CALENDAR_DATE, date };
+};
 
 export default function sessionDataReducer(state = {}, action) {
     switch (action.type) {
@@ -17,6 +27,12 @@ export default function sessionDataReducer(state = {}, action) {
             return { ...state, year: action.year };
         case SET_MONTH:
             return { ...state, month: action.month };
+        case SET_SIDE_CALENDAR_YEAR:
+            return { ...state, sideCalendarYear: action.year };
+        case SET_SIDE_CALENDAR_MONTH:
+            return { ...state, sideCalendarMonth: action.month };
+        case SET_SIDE_CALENDAR_DATE:
+            return { ...state, sideCalendarDate: action.date };
         default:
             return state;
     }
