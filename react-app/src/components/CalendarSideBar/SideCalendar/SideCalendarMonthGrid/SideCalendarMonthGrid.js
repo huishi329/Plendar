62ad30b506252959ffb5d86943560bc1771b53fc
Huishi <anhuishi95@gmail.com>
2023-01-08 12:09:31 -0800

    Add SideCalendarMonthGrid

---
+++ react-app/src/components/CalendarSideBar/SideCalendar/SideCalendarMonthGrid/SideCalendarMonthGrid.js
@@ -0,0 +1,16 @@
+import MonthDay from "./MonthDay/MonthDay";
+import styles from './SideCalendarMonthGrid.module.css'
+
+export default function SideCalendarMonthView({ year, month }) {
+    const firstDateOfMonth = new Date(year, month);
+    const firstDayOfMonth = firstDateOfMonth.getDay();
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.monthGrid}>
+                {[...Array(42)].map((_, idx) => {
+                    return <MonthDay date={new Date(year, month, idx - firstDayOfMonth + 1)} key={idx + 1} />
+                })}
+            </div>
+        </div>
+    )
+}
