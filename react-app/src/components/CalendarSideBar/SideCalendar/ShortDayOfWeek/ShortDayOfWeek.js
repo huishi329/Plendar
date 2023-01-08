8a17d95ccc43b0efeed282bbf17f8bcbee75fa8b
Huishi <anhuishi95@gmail.com>
2023-01-08 11:48:08 -0800

    Add ShortDayOfWeek

---
+++ react-app/src/components/CalendarSideBar/SideCalendar/ShortDayOfWeek/ShortDayOfWeek.js
@@ -0,0 +1,15 @@
+import styles from './ShortDayOfWeek.module.css'
+
+export default function ShortDayOfWeek() {
+    return (
+        <div className={styles.wrapper}>
+            <div>S</div>
+            <div>M</div>
+            <div>T</div>
+            <div>W</div>
+            <div>T</div>
+            <div>F</div>
+            <div>S</div>
+        </div>
+    )
+}
