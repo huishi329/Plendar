ae5d870b607000d7dd0a5890b8c7a2ddc601e385
Huishi <anhuishi95@gmail.com>
2023-01-08 20:05:36 -0800

    Add empty array for toLocale method

--- react-app/src/components/EventForm/EventForm.js
+++ react-app/src/components/EventForm/EventForm.js
@@ -14,14 +14,15 @@ export default function EventForm({ date, x, y }) {
     const calendarsOwned = calendarsArr?.filter(calendar => calendar.owner_id === user.id);
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-    const dateStr = date.toLocaleDateString({ year: "numeric", month: "2-digit", day: "2-digit" }, { timezone: timezone }).split("/").reverse().join("-");
-    const [currentHour, currentMinute] = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", timezone: timezone }).split(":");
+    const dateStr = date.toLocaleDateString([], { year: "numeric", month: "2-digit", day: "2-digit" }, { timeZone: timezone }).split("/").reverse().join("-");
+    console.log(date.toLocaleDateString([], { year: "numeric", month: "2-digit", day: "2-digit" }));
+    const [currentHour, currentMinute] = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", timeZone: timezone }).split(":");
     const startTimeStr = (currentMinute < 30) ?
         `${currentHour}:30`
-        : `${((Number(currentHour) + 1) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false, timezone: timezone })}:00`
+        : `${((Number(currentHour) + 1) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false, timeZone: timezone })}:00`
     const endTimeStr = (currentMinute < 30) ?
-        `${((Number(currentHour) + 1) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false, timezone: timezone })}:30`
-        : `${((Number(currentHour) + 2) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false, timezone: timezone })}:00`
+        `${((Number(currentHour) + 1) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false, timeZone: timezone })}:30`
+        : `${((Number(currentHour) + 2) % 24).toLocaleString([], { minimumIntegerDigits: 2, useGrouping: false, timeZone: timezone })}:00`
 
     const [expandTimeOptions, setExpandTimeOptions] = useState(false);
     const [expandMoreOptions, setExpanMoreOptions] = useState(false);
@@ -117,7 +118,7 @@ export default function EventForm({ date, x, y }) {
                                             (new Date().setHours(...e.target.value.split(":")))) {
                                             const tomorrow = new Date(date);
                                             tomorrow.setDate(tomorrow.getDate() + 1);
-                                            const endDateStr = tomorrow.toLocaleDateString({ year: "numeric", month: "2-digit", day: "2-digit", timezone: timezone }).split("/").reverse().join("-");
+                                            const endDateStr = tomorrow.toLocaleDateString({ year: "numeric", month: "2-digit", day: "2-digit", timeZone: timezone }).split("/").reverse().join("-");
                                             setEndDate(endDateStr);
                                         }
                                     }}
@@ -159,7 +160,7 @@ export default function EventForm({ date, x, y }) {
                         <option value={0}>Doesn't repeat</option>
                         <option value={1}>Every day</option>
                         <option value={5}>Every weekday</option>
-                        <option value={7}>Weekly on {date.toLocaleDateString('en-US', { weekday: 'long', timezone: timezone })}</option>
+                        <option value={7}>Weekly on {date.toLocaleDateString('en-US', { weekday: 'long', timeZone: timezone })}</option>
                     </select>
                 </div>}
             <div className={styles.address}>
