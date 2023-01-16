31a438a1161ac8d591378bd4b7825d5c057c1857
Huishi <anhuishi95@gmail.com>
2023-01-16 15:57:02 -0800

    Add update_event_status route

--- app/api/events_guests.py
+++ app/api/events_guests.py
@@ -6,7 +6,12 @@ from app.forms import validation_errors_formatter
 bp = Blueprint('events_guests', __name__, url_prefix="/events_guests")
 
 
-@bp.route("", methods=["POST"])
+@bp.route("/<int:event_id>", methods=["PATCH"])
 @login_required
-def add_event_guest():
-    pass
+def update_event_status(event_id):
+    status = request.json['status']
+    event_guest = EventGuest.query.filter(
+        EventGuest.event_id == event_id, EventGuest.guest_id == current_user.id).one()
+    event_guest.status = status
+    db.session.commit()
+    return {"message": f"Guest {current_user.id} responded {status} to event {event_id}"}
