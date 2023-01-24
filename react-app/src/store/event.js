4a720020f656f67f5e4494667112337e75d259ee
Huishi <anhuishi95@gmail.com>
2023-01-23 19:27:43 -0800

    Add actions

--- react-app/src/store/event.js
+++ react-app/src/store/event.js
@@ -1,6 +1,6 @@
 import { csrfFetch } from './csrf';
 
-const GET_EVENT = 'events/:eventId';
+const GET_EVENT = 'event/getEvent';
 
 export const getEvent = (eventId) => async dispatch => {
     const response = await csrfFetch(`/api/events/${eventId}`)
@@ -8,10 +8,40 @@ export const getEvent = (eventId) => async dispatch => {
     dispatch({ type: GET_EVENT, event });
 }
 
+const UPDATE_TENTATIVE_EVENT_STATUS = 'event/updateTentativeEventGuestStatus';
+
+export const updateTentativeEventStatus = (userId, status) => {
+    return { type: UPDATE_TENTATIVE_EVENT_STATUS, userId, status }
+}
+
+const ADD_GUEST = 'event/addGuest';
+
+export const addGuest = (email) => async dispatch => {
+    const response = await csrfFetch(`/api/users?email=${email}`);
+    const guest = response.json();
+
+    dispatch({ type: ADD_GUEST, guest })
+};
+
+const CLEAN_EVENT = 'event/cleanEvent';
+
+export const cleanEvent = () => {
+    return { type: CLEAN_EVENT }
+}
+
 export default function eventReducer(state = null, action) {
     const newState = { ...state };
     switch (action.type) {
         case GET_EVENT:
-            return action.event
+            return action.event;
+        case UPDATE_TENTATIVE_EVENT_STATUS:
+            newState.guests[action.userId].status = action.status;
+            return newState;
+        // case ADD_GUEST:
+        // // if ()
+        case CLEAN_EVENT:
+            return null;
+        default:
+            return state;
     }
 }
