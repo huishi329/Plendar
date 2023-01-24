1cea0b2411af813895e92614d912e8dda9fa044c
Huishi <anhuishi95@gmail.com>
2023-01-23 22:45:27 -0800

    Add addGuest thunk action

--- react-app/src/store/event.js
+++ react-app/src/store/event.js
@@ -16,11 +16,11 @@ export const updateTentativeEventStatus = (userId, status) => {
 
 const ADD_GUEST = 'event/addGuest';
 
-export const addGuest = (email) => async dispatch => {
+export const addGuest = (email, user) => async dispatch => {
     const response = await csrfFetch(`/api/users?email=${email}`);
-    const guest = response.json();
+    const guest = await response.json();
 
-    dispatch({ type: ADD_GUEST, guest })
+    dispatch({ type: ADD_GUEST, guest, user })
 };
 
 const CLEAN_EVENT = 'event/cleanEvent';
@@ -37,8 +37,13 @@ export default function eventReducer(state = null, action) {
         case UPDATE_TENTATIVE_EVENT_STATUS:
             newState.guests[action.userId].status = action.status;
             return newState;
-        // case ADD_GUEST:
-        // // if ()
+        case ADD_GUEST:
+            if (!newState.guests || Object.values(newState.guests) === 0) {
+                newState.guests = {};
+                newState.guests[action.user.id] = Object.assign(action.user, { status: 'yes' })
+            }
+            newState.guests[action.guest.id] = Object.assign(action.guest, { status: 'awaiting' })
+            return newState
         case CLEAN_EVENT:
             return null;
         default:
