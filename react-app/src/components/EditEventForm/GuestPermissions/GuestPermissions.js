17f03473906f32ac5db0f95f7bb0421904e3883d
Huishi <anhuishi95@gmail.com>
2023-01-26 15:36:40 -0800

    Make checkbox inputs controllable

--- react-app/src/components/EditEventForm/GuestPermissions/GuestPermissions.js
+++ react-app/src/components/EditEventForm/GuestPermissions/GuestPermissions.js
@@ -1,6 +1,12 @@
 import styles from './GuestPermissions.module.css'
+import { useState } from 'react';
 
-export default function GuestPermission() {
+
+export default function GuestPermission({ event }) {
+    const [modifyEvent, setModifyEvent] = useState(event.guest_modify_event);
+    const [inviteOthers, setInviteOthers] = useState(event.guest_invite_others);
+    const [seeGuestList, setSeeGuestList] = useState(event.guest_see_guest_list);
+
     return (
         <div className={styles.wrapper}>
             <div>Guest Permission</div>
@@ -8,6 +14,8 @@ export default function GuestPermission() {
                 <input
                     type='checkbox'
                     name='modifyEvent'
+                    checked={modifyEvent}
+                    onChange={() => setModifyEvent(!modifyEvent)}
                 ></input>
                 <label htmlFor='modifyEvent'>Modify event</label>
             </div>
@@ -15,6 +23,8 @@ export default function GuestPermission() {
                 <input
                     type='checkbox'
                     name='inviteOthers'
+                    checked={inviteOthers}
+                    onChange={() => setInviteOthers(!inviteOthers)}
                 ></input>
                 <label htmlFor='inviteOthers'>Invite others</label>
             </div>
@@ -22,6 +32,8 @@ export default function GuestPermission() {
                 <input
                     type='checkbox'
                     name='seeGuestList'
+                    checked={seeGuestList}
+                    onChange={() => setSeeGuestList(!seeGuestList)}
                 ></input>
                 <label htmlFor='seeGuestList'>See guest list</label>
             </div>
