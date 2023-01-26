f3b55cc03a84e16cdd6b78defb928c8a45557f19
Huishi <anhuishi95@gmail.com>
2023-01-26 13:19:59 -0800

    Delete print statement

--- app/api/users_calendars.py
+++ app/api/users_calendars.py
@@ -4,13 +4,13 @@ from app.models import db, User, Calendar, UserCalendar
 
 bp = Blueprint('users_calendars', __name__, url_prefix="/users_calendars")
 
+
 @bp.route("/current/<int:calendar_id>", methods=['PATCH'])
 @login_required
 def toggle_calendar(calendar_id):
     user_calendar = UserCalendar.query.filter(
         UserCalendar.user_id == current_user.id,
-        UserCalendar.calendar_id == calendar_id).first();
-    print(user_calendar)
+        UserCalendar.calendar_id == calendar_id).first()
     user_calendar.is_displayed = not user_calendar.is_displayed
     db.session.commit()
     return user_calendar.to_dict()
