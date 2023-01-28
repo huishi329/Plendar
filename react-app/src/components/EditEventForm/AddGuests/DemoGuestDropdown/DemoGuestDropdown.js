a0406c08cd6a12f214a6cb545fd64d05f8a52625
Huishi <anhuishi95@gmail.com>
2023-01-28 12:07:03 -0800

    Add DemoGuestDropdown

---
+++ react-app/src/components/EditEventForm/AddGuests/DemoGuestDropdown/DemoGuestDropdown.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+import { addGuest } from '../../../../store/event'
+import styles from './DemoGuestDropdown.module.css'
+import { demoGuests } from './demoGuests'
+
+export default function DemoGuestDropdown({ user }) {
+
+    return (
+        <div className={styles.wrapper}>
+            {demoGuests.map(guest => (
+                < div className={styles.guest} key={guest.name}
+                    onClick={() => addGuest(guest.email, user)}>
+                    <div className={styles.image}>
+                        <img src={guest.profile_picture_url} alt={guest.name}></img>
+                    </div>
+                    <div>
+                        <div className={styles.boldText}>{guest.name}</div>
+                        <div className={styles.boldText}>{guest.email}</div>
+                    </div>
+                </div >)
+            )}
+
+        </div>
+
+    )
+}
---
+++ react-app/src/components/EditEventForm/AddGuests/DemoGuestDropdown/demoGuests.js
@@ -0,0 +1,5 @@
+export const demoGuests = [
+    { name: 'Demo', email: 'demo@plendar.io', profile_picture_url: "https://plendar.s3.us-west-2.amazonaws.com/plendar.png" },
+    { name: 'Marnie', email: 'marnie@plendar.io', profile_picture_url: "https://plendar.s3.us-west-2.amazonaws.com/plendar.png" },
+    { name: 'Bobbie', email: 'bobbie@plendar.io', profile_picture_url: "https://plendar.s3.us-west-2.amazonaws.com/plendar.png" },
+];
