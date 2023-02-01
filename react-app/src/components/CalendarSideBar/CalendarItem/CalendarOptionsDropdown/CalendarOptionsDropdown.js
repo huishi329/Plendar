fcd3be3add3a039e489ee72fcd19c85648056aa7
Huishi <anhuishi95@gmail.com>
2023-02-01 13:52:16 -0800

    Add CalendarColors

--- react-app/src/components/CalendarSideBar/CalendarItem/CalendarOptionsDropdown/CalendarOptionsDropdown.js
+++ react-app/src/components/CalendarSideBar/CalendarItem/CalendarOptionsDropdown/CalendarOptionsDropdown.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './CalendarOptionsDropdown.module.css'
+import { colors } from './CalendarColors'
 
-export default function CalendarOptionsDropdown({ calendar, user }) {
+export default function CalendarOptionsDropdown({ calendar, user, setShowCalendarOptionsDropdown }) {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const closeDropdown = () => setShowCalendarOptionsDropdown(false);
+        document.addEventListener('click', closeDropdown);
+        return () => document.removeEventListener('click', closeDropdown)
+    }, [setShowCalendarOptionsDropdown])
 
     const handleEdit = (e) => {
         e.stopPropagation();
@@ -12,8 +20,11 @@ export default function CalendarOptionsDropdown({ calendar, user }) {
 
         <div className={styles.wrapper}>
             {calendar.owner_id === user.id &&
-                <div className={styles} onClick={handleEdit}>Settings</div>}
-            <div className={styles.colors}></div>
+                <div className={styles.settings} onClick={handleEdit}>Settings</div>}
+            <div className={styles.colors}>
+                {colors.map(color => (
+                    <button key={color} className={styles.colorButton} style={{ backgroundColor: `${color}` }}></button>))}
+            </div>
         </div>
     )
 }
