57220646d73f716c1395bb6c37d809036e86e01b
Huishi <anhuishi95@gmail.com>
2023-02-01 14:05:15 -0800

    Dispatch change of color for calendar

--- react-app/src/components/CalendarSideBar/CalendarItem/CalendarOptionsDropdown/CalendarOptionsDropdown.js
+++ react-app/src/components/CalendarSideBar/CalendarItem/CalendarOptionsDropdown/CalendarOptionsDropdown.js
@@ -1,10 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, } from 'react';
+import { useDispatch } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import styles from './CalendarOptionsDropdown.module.css'
 import { colors } from './CalendarColors'
+import { changeCalendarColor } from '../../../../store/calendars';
 
 export default function CalendarOptionsDropdown({ calendar, user, setShowCalendarOptionsDropdown }) {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const closeDropdown = () => setShowCalendarOptionsDropdown(false);
@@ -23,7 +26,11 @@ export default function CalendarOptionsDropdown({ calendar, user, setShowCalenda
                 <div className={styles.settings} onClick={handleEdit}>Settings</div>}
             <div className={styles.colors}>
                 {colors.map(color => (
-                    <button key={color} className={styles.colorButton} style={{ backgroundColor: `${color}` }}></button>))}
+                    <button
+                        key={color}
+                        className={styles.colorButton}
+                        style={{ backgroundColor: `${color}` }}
+                        onClick={() => dispatch(changeCalendarColor(calendar.id, color))}></button>))}
             </div>
         </div>
     )
