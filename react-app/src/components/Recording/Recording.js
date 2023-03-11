4316dfe5a4d0f96fbdef447248fedef0d99303f0
Huishi <anhuishi95@gmail.com>
2023-03-11 14:22:39 -0800

    Add Recording button to CalendarSideBar

---
+++ react-app/src/components/Recording/Recording.js
@@ -0,0 +1,9 @@
+import styles from './Recording.module.css';
+
+export function Recording () {
+    return (
+        <div className={styles.container}>
+          <i className="fa-solid fa-microphone"></i>
+        </div>
+    );
+}
